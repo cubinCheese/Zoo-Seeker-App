739ce4c07dcf72bf3919561e98b79d890632c61e
Brian Lu <brianlu09@gmail.com>
2022-05-01 14:57:46 -0700

    added empty test files (#26)

---
+++ app/src/test/java/com/example/project_110/VertexListTest.java
@@ -0,0 +1,27 @@
+package com.example.project_110;
+
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RunWith(AndroidJUnit4.class)
+public class VertexListTest {
+
+    @Before
+    public void build_vList(){
+
+    }
+
+    @Test
+    public void test_makeMap(){
+
+    }
+
+}
