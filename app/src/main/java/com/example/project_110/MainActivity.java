2ad101d5b965570a84691f8995cd39e6f76fd337
William Jin <wjin0702@yahoo.com>
2022-04-30 19:20:34 -0700

    Added Search Bar (#27)

--- app/src/main/java/com/example/project_110/MainActivity.java
+++ app/src/main/java/com/example/project_110/MainActivity.java
@@ -3,11 +3,17 @@ package com.example.project_110;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+
 import android.os.Bundle;
+import android.widget.SearchView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
+
+    SearchBar searchbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,12 +21,15 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         // initializing new Map searchable by tags // & list of vertex info
-      //  Map<String, ZooData.VertexInfo> vInfo = ZooData.loadVertexInfoJSON(this, "sample_node_info.json");
-       // VertexList vertexList = new VertexList(vInfo);
+        Map<String, ZooData.VertexInfo> vInfo = ZooData.loadVertexInfoJSON(this, "sample_node_info.json");
+        VertexList vertexList = new VertexList(vInfo);
 
 
         Intent intent = new Intent(this, SearchDisplayActivity.class);
         startActivity(intent);
+
+        SearchView search = (SearchView) findViewById(R.id.search);
+        searchbar = new SearchBar(search);
     }
 
 }
