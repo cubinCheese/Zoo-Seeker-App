310d2c7961ea0e8523c3b3be4bdf2291257d0fa4
William Jin <wjin0702@yahoo.com>
2022-05-01 22:52:40 -0700

    Fix compile errors of unit test

--- app/src/main/java/com/example/project_110/MainActivity.java
+++ app/src/main/java/com/example/project_110/MainActivity.java
@@ -1,15 +1,9 @@
 package com.example.project_110;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Intent;
-
 import android.os.Bundle;
 import android.widget.SearchView;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,12 +14,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         // initializing new Map searchable by tags // & list of vertex info
-        Map<String, ZooData.VertexInfo> vInfo = ZooData.loadVertexInfoJSON(this, "sample_node_info.json");
-        VertexList vertexList = new VertexList(vInfo);
+        //Map<String, ZooData.VertexInfo> vInfo = ZooData.loadVertexInfoJSON(this, "sample_node_info.json");
+        //VertexList vertexList = new VertexList(vInfo);
 
 
-        Intent intent = new Intent(this, SearchDisplayActivity.class);
-        startActivity(intent);
+        // Intent intent = new Intent(this, SearchDisplayActivity.class);
+        // startActivity(intent);
 
         SearchView search = (SearchView) findViewById(R.id.search);
         searchbar = new SearchBar(search);
