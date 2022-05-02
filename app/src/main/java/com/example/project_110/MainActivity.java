03b26dcd6bf92bbcca799170c8572ba0cfc763da
William Jin <wjin0702@yahoo.com>
2022-05-01 23:34:17 -0700

    Connect searchbar to vertex list and adapter (#27)

--- app/src/main/java/com/example/project_110/MainActivity.java
+++ app/src/main/java/com/example/project_110/MainActivity.java
@@ -1,29 +1,19 @@
 package com.example.project_110;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.widget.SearchView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-
-    SearchBar searchbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // initializing new Map searchable by tags // & list of vertex info
-        //Map<String, ZooData.VertexInfo> vInfo = ZooData.loadVertexInfoJSON(this, "sample_node_info.json");
-        //VertexList vertexList = new VertexList(vInfo);
-
-
-        // Intent intent = new Intent(this, SearchDisplayActivity.class);
-        // startActivity(intent);
-
-        SearchView search = (SearchView) findViewById(R.id.search);
-        searchbar = new SearchBar(search);
+        Intent intent = new Intent(this, SearchDisplayActivity.class);
+        startActivity(intent);
     }
 
 }
--- app/src/main/java/com/example/project_110/SearchBar.java
+++ app/src/main/java/com/example/project_110/SearchBar.java
@@ -2,12 +2,24 @@ package com.example.project_110;
 
 import android.widget.SearchView;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class SearchBar implements SearchView.OnQueryTextListener {
     public SearchView search;
     private String currentQuery;
+    public VertexList vertexList;
+    public List<ZooData.VertexInfo> currentAnimalsFromQuery;
 
-    public SearchBar(SearchView search) {
+    public SearchBar(SearchView search, VertexList vertexList) {
         this.search = search;
+        this.vertexList = vertexList;
+        currentAnimalsFromQuery = new ArrayList<ZooData.VertexInfo>();
+        List<String> grizzList = new ArrayList();
+        grizzList.add("grizzly");
+        grizzList.add("bear");
+        grizzList.add("mammal");
+        currentAnimalsFromQuery.add(new ZooData.VertexInfo("grizzly_bears", ZooData.VertexInfo.Kind.EXHIBIT, "Grizzly Bears", grizzList));
         search.setOnQueryTextListener(this);
     }
 
@@ -19,7 +31,7 @@ public class SearchBar implements SearchView.OnQueryTextListener {
     @Override
     public boolean onQueryTextChange(String s) {
         this.currentQuery = s;
-        //PUT SEARCH METHOD HERE
+        currentAnimalsFromQuery = vertexList.search(s);
         return false;
     }
 
