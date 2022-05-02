1f27b45205342e6b99dedba1849187c930595791
William Jin <wjin0702@yahoo.com>
2022-05-01 22:02:21 -0700

    Add searchbar tests (#27)

---
+++ app/src/test/java/com/example/project_110/SearchBarUnitTest.java
@@ -0,0 +1,26 @@
+package com.example.project_110;
+
+import static org.junit.Assert.assertEquals;
+
+import android.widget.SearchView;
+
+import androidx.lifecycle.Lifecycle;
+import androidx.test.core.app.ActivityScenario;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(AndroidJUnit4.class)
+public class SearchBarUnitTest {
+    @Test
+    public void getsCorrectText() {
+        ActivityScenario<MainActivity> scenario = ActivityScenario.launch((MainActivity.class));
+        scenario.moveToState(Lifecycle.State.CREATED);
+        scenario.onActivity(activity -> {
+            SearchView searchView = (SearchView) activity.findViewById(R.id.search);
+            searchView.setQuery("Joe", true);
+            assertEquals(activity.searchbar.getCurrentQuery(), "Joe");
+        });
+    }
+}
