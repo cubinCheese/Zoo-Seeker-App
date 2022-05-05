a3513e1adec468f42118fbd25ce4f04125b052ce
Brian Lu <brianlu09@gmail.com>
2022-05-04 21:49:49 -0700

    Most updated code (#24)

--- app/src/main/java/com/example/project_110/ZooData.java
+++ app/src/main/java/com/example/project_110/ZooData.java
@@ -11,6 +11,10 @@ import com.google.gson.Gson;
 import com.google.gson.annotations.SerializedName;
 import com.google.gson.reflect.TypeToken;
 
+import org.jgrapht.Graph;
+import org.jgrapht.graph.DefaultUndirectedWeightedGraph;
+import org.jgrapht.nio.json.JSONImporter;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -96,28 +100,28 @@ public class ZooData {
         return indexedZooData;
     }
 
-//    public static Graph<String, IdentifiedWeightedEdge> loadZooGraphJSON(String path) {
-//        // Create an empty graph to populate.
-//        Graph<String, IdentifiedWeightedEdge> g = new DefaultUndirectedWeightedGraph<>(IdentifiedWeightedEdge.class);
-//
-//        // Create an importer that can be used to populate our empty graph.
-//        JSONImporter<String, IdentifiedWeightedEdge> importer = new JSONImporter<>();
-//
-//        // We don't need to convert the vertices in the graph, so we return them as is.
-//        importer.setVertexFactory(v -> v);
-//
-//        // We need to make sure we set the IDs on our edges from the 'id' attribute.
-//        // While this is automatic for vertices, it isn't for edges. We keep the
-//        // definition of this in the IdentifiedWeightedEdge class for convenience.
-//        importer.addEdgeAttributeConsumer(IdentifiedWeightedEdge::attributeConsumer);
-//
-//        // On Android, you would use context.getAssets().open(path) here like in Lab 5.
-//        InputStream inputStream = MainActivity.class.getClassLoader().getResourceAsStream(path);
-//        Reader reader = new InputStreamReader(inputStream);
-//
-//        // And now we just import it!
-//        importer.importGraph(g, reader);
-//
-//        return g;
-//    }
+    public static Graph<String, IdentifiedWeightedEdge> loadZooGraphJSON(String path) {
+        // Create an empty graph to populate.
+        Graph<String, IdentifiedWeightedEdge> g = new DefaultUndirectedWeightedGraph<>(IdentifiedWeightedEdge.class);
+
+        // Create an importer that can be used to populate our empty graph.
+        JSONImporter<String, IdentifiedWeightedEdge> importer = new JSONImporter<>();
+
+        // We don't need to convert the vertices in the graph, so we return them as is.
+        importer.setVertexFactory(v -> v);
+
+        // We need to make sure we set the IDs on our edges from the 'id' attribute.
+        // While this is automatic for vertices, it isn't for edges. We keep the
+        // definition of this in the IdentifiedWeightedEdge class for convenience.
+        importer.addEdgeAttributeConsumer(IdentifiedWeightedEdge::attributeConsumer);
+
+        // On Android, you would use context.getAssets().open(path) here like in Lab 5.
+        InputStream inputStream = MainActivity.class.getClassLoader().getResourceAsStream(path);
+        Reader reader = new InputStreamReader(inputStream);
+
+        // And now we just import it!
+        importer.importGraph(g, reader);
+
+        return g;
+    }
 }
