d223de66816522b0e4948e17221398fb3d9adf83
Your Name <you@example.com>
2022-05-05 20:59:59 -0700

    new_exhibit_page #62

---
+++ app/src/main/java/com/example/project_110/FirstNewExhibitActivity.java
@@ -0,0 +1,14 @@
+package com.example.project_110;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FirstNewExhibitActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_first_new_exhibit);
+    }
+}
