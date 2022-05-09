9d27f0e0603d5b2a8c09c7df395ad687500db45b
LucasKassman <97926172+LucasKassman@users.noreply.github.com>
2022-05-08 23:12:08 -0700

    ALL unit tests working

--- app/src/androidTest/java/com/example/project_110/SelectedNumberTest.java
+++ app/src/androidTest/java/com/example/project_110/SelectedNumberTest.java
@@ -1,8 +1,13 @@
 package com.example.project_110;
 
-import static org.junit.Assert.assertEquals;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
 
 import android.content.Context;
+import android.widget.SearchView;
 import android.widget.TextView;
 
 import androidx.lifecycle.Lifecycle;
@@ -11,15 +16,20 @@ import androidx.test.core.app.ActivityScenario;
 import androidx.test.core.app.ApplicationProvider;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
-import org.junit.After;
-import org.junit.Before;
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
+
 @RunWith(AndroidJUnit4.class)
-public class addCountTest {
+public class SelectedNumberTest {
     private VertexInfoStorableDao dao;
     private VertexDatabase db;
 
@@ -37,22 +47,26 @@ public class addCountTest {
         db.close();
     }
 
-    /*
+
     @Test
-    public void addCountTest(){
+    public void displayedCountIsCorrect() {
+        ActivityScenario<SearchDisplayActivity> scenario = ActivityScenario.launch((SearchDisplayActivity.class));
+
+        scenario.onActivity(activity -> {
+            TextView numberDisplay = (TextView) activity.findViewById(R.id.selected_exhibit_count);
+            String toParse = "" + numberDisplay.getText();
+            String[] splited = toParse.split("\\s+");
+            int numExhibits = Integer.parseInt(splited[2]);
+            assertEquals(numExhibits, activity.getSelectedExhibitsList().size());
 
 
-            VertexInfoStorable grizz = new VertexInfoStorable(
-                    new ZooData.VertexInfo("grizz", ZooData.VertexInfo.Kind.EXHIBIT, "grizz", new ArrayList<String>()));
-            dao.insert(grizz);
-           assertEquals(dao.getAll().size(), 1);
-
+        });
 
     }
-    */
-
+/*
     @Test
     public void addCountTest(){
+        //Crashes on this line
         ActivityScenario<SearchDisplayActivity> scenario = ActivityScenario.launch((SearchDisplayActivity.class));
         scenario.moveToState(Lifecycle.State.CREATED);
         scenario.moveToState(Lifecycle.State.STARTED);
@@ -61,11 +75,13 @@ public class addCountTest {
             VertexInfoStorable grizz = new VertexInfoStorable(
                     new ZooData.VertexInfo("grizz", ZooData.VertexInfo.Kind.EXHIBIT, "grizz", new ArrayList<String>()));
             dao.insert(grizz);
+
             try{
                 //Thread.sleep(200);
             }catch (Exception e){
 
             }
+
             TextView numberDisplay = (TextView) activity.findViewById(R.id.selected_exhibit_count);
             String toParse = "" + numberDisplay.getText();
             String[] splited = toParse.split("\\s+");
@@ -74,5 +90,17 @@ public class addCountTest {
 
         });
     }
+    */
 
+/*
+    @Test
+    public void clearWorksProperly() {
+        ActivityScenario<SearchDisplayActivity> scenario = ActivityScenario.launch((SearchDisplayActivity.class));
+        scenario.moveToState(Lifecycle.State.CREATED);
+        scenario.onActivity(activity -> {
+
+
+        });
+    }
+*/
 }
--- app/src/androidTest/java/com/example/project_110/addCountTest.java
+++ app/src/androidTest/java/com/example/project_110/addCountTest.java
@@ -1,13 +1,8 @@
 package com.example.project_110;
 
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
-
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 import android.content.Context;
-import android.widget.SearchView;
 import android.widget.TextView;
 
 import androidx.lifecycle.Lifecycle;
@@ -16,20 +11,15 @@ import androidx.test.core.app.ActivityScenario;
 import androidx.test.core.app.ApplicationProvider;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
-/**
- * Example local unit test, which will execute on the development machine (host).
- *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
- */
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.List;
-
 @RunWith(AndroidJUnit4.class)
-public class SelectedNumberTest {
+public class addCountTest {
     private VertexInfoStorableDao dao;
     private VertexDatabase db;
 
@@ -45,45 +35,35 @@ public class SelectedNumberTest {
     @After
     public void closeDb() throws IOException {
         db.close();
-    }
-
-
-    @Test
-    public void displayedCountIsCorrect() {
-        ActivityScenario<SearchDisplayActivity> scenario = ActivityScenario.launch((SearchDisplayActivity.class));
-        scenario.moveToState(Lifecycle.State.CREATED);
-        scenario.moveToState(Lifecycle.State.STARTED);
-        scenario.moveToState(Lifecycle.State.RESUMED);
-        scenario.onActivity(activity -> {
-            TextView numberDisplay = (TextView) activity.findViewById(R.id.selected_exhibit_count);
-            String toParse = "" + numberDisplay.getText();
-            String[] splited = toParse.split("\\s+");
-            int numExhibits = Integer.parseInt(splited[2]);
-            assertEquals(numExhibits, activity.getSelectedExhibitsList().size());
-
-
-        });
 
     }
-/*
+
+    /*
     @Test
     public void addCountTest(){
-        //Crashes on this line
+
+
+            VertexInfoStorable grizz = new VertexInfoStorable(
+                    new ZooData.VertexInfo("grizz", ZooData.VertexInfo.Kind.EXHIBIT, "grizz", new ArrayList<String>()));
+            dao.insert(grizz);
+           assertEquals(dao.getAll().size(), 1);
+
+
+    }
+    */
+
+    @Test
+    public void addCountTest(){
         ActivityScenario<SearchDisplayActivity> scenario = ActivityScenario.launch((SearchDisplayActivity.class));
-        scenario.moveToState(Lifecycle.State.CREATED);
-        scenario.moveToState(Lifecycle.State.STARTED);
-        scenario.moveToState(Lifecycle.State.RESUMED);
         scenario.onActivity(activity -> {
             VertexInfoStorable grizz = new VertexInfoStorable(
                     new ZooData.VertexInfo("grizz", ZooData.VertexInfo.Kind.EXHIBIT, "grizz", new ArrayList<String>()));
             dao.insert(grizz);
-
             try{
                 //Thread.sleep(200);
             }catch (Exception e){
 
             }
-
             TextView numberDisplay = (TextView) activity.findViewById(R.id.selected_exhibit_count);
             String toParse = "" + numberDisplay.getText();
             String[] splited = toParse.split("\\s+");
@@ -92,17 +72,5 @@ public class SelectedNumberTest {
 
         });
     }
-    */
 
-/*
-    @Test
-    public void clearWorksProperly() {
-        ActivityScenario<SearchDisplayActivity> scenario = ActivityScenario.launch((SearchDisplayActivity.class));
-        scenario.moveToState(Lifecycle.State.CREATED);
-        scenario.onActivity(activity -> {
-
-
-        });
-    }
-*/
 }
