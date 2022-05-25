200c1a62fafe3da50af7f0cca466ee9b4c5fabc6
LucasKassman <97926172+LucasKassman@users.noreply.github.com>
2022-05-24 23:29:07 -0700

    Added working tests

--- app/src/androidTest/java/com/example/project_110/SelectedDisplayTest.java
+++ app/src/androidTest/java/com/example/project_110/SelectedDisplayTest.java
@@ -11,6 +11,7 @@ import android.widget.SearchView;
 import android.widget.TextView;
 
 import androidx.lifecycle.Lifecycle;
+import androidx.recyclerview.widget.RecyclerView;
 import androidx.room.Room;
 import androidx.test.core.app.ActivityScenario;
 import androidx.test.core.app.ApplicationProvider;
@@ -46,16 +47,42 @@ public class SelectedDisplayTest {
     public void closeDb() throws IOException {
         db.close();
     }
+}
 
-
+/*
     @Test
-    public void selectedNumberCorrect(){
+    public void selectedDisplayCorrect(){
+
+        ActivityScenario<SearchDisplayActivity> scenario = ActivityScenario.launch((SearchDisplayActivity.class));
+        scenario.onActivity(activity -> {
+            VertexInfoStorable grizz = new VertexInfoStorable(
+                    new ZooData.VertexInfo("grizz", ZooData.VertexInfo.Kind.EXHIBIT, "grizz", new ArrayList<String>()));
+            dao.insert(grizz);
+            try{
+                Thread.sleep(400);
+            }catch (Exception e){
+
+            }
+            assertEquals(1,dao.getAll().size());
+            TextView numberDisplay = (TextView) activity.findViewById(R.id.selected_exhibit_count);
+            String toParse = "" + numberDisplay.getText();
+            String[] splited = toParse.split("\\s+");
+            int numExhibits = Integer.parseInt(splited[2]);
+            assertEquals(numExhibits, activity.getSelectedExhibitsList().size());
+            RecyclerView recyclerView = activity.findViewById(R.id.selected_items);
+            int x = recyclerView.getChildCount();
+            assertEquals(x,1);
+            TextView grizzCheck = (TextView)recyclerView.getChildAt(x).findViewById(R.id.selected_list_item);
+            String nameCheck = grizzCheck.getText().toString();
+            assertEquals(nameCheck, grizz.name);
+
+        });
 
     }
 
 
 }
-
+*/
 
 
 
---
+++ app/src/test/java/com/example/project_110/ItemCountTest.java
@@ -0,0 +1,59 @@
+package com.example.project_110;
+
+import static org.junit.Assert.assertEquals;
+
+import android.content.Context;
+import android.widget.TextView;
+
+import androidx.lifecycle.Lifecycle;
+import androidx.room.Room;
+import androidx.test.core.app.ActivityScenario;
+import androidx.test.core.app.ApplicationProvider;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+@RunWith(AndroidJUnit4.class)
+public class ItemCountTest {
+
+
+
+
+    @Test
+   public void itemCountTest(){
+
+        SelectedDisplayAdapter selectedDisplayAdapter = new SelectedDisplayAdapter();
+        List<VertexInfoStorable> addList = new ArrayList<VertexInfoStorable>();
+        addList.add(new VertexInfoStorable(
+                new ZooData.VertexInfo("grizz", ZooData.VertexInfo.Kind.EXHIBIT, "grizz", new ArrayList<String>())));
+
+        selectedDisplayAdapter.setSelectedExhibits(addList);
+        assertEquals(selectedDisplayAdapter.getItemCount(),1);
+
+    }
+
+
+    @Test
+    public void removeCountTest(){
+
+        SelectedDisplayAdapter selectedDisplayAdapter = new SelectedDisplayAdapter();
+        List<VertexInfoStorable> addList = new ArrayList<VertexInfoStorable>();
+        addList.add(new VertexInfoStorable(
+                new ZooData.VertexInfo("grizz", ZooData.VertexInfo.Kind.EXHIBIT, "grizz", new ArrayList<String>())));
+
+        selectedDisplayAdapter.setSelectedExhibits(addList);
+        selectedDisplayAdapter.setSelectedExhibits(Collections.emptyList());
+        assertEquals(selectedDisplayAdapter.getItemCount(),0);
+    }
+
+
+
+}
